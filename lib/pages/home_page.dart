5feba734c5f0442baebc209ad5fbe442513a67c7
Raed <raed.dev@gmail.com>
2025-01-30 10:17:35 +0000

    modifying home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,7 +30,7 @@ class _HomePageState extends State<HomePage> {
           alignment: const Alignment(0.0, 0.0),
           children: [
             Positioned(
-              top: 29.0,
+              top: 86.0,
               left: 42.5,
               width: 308.0,
               height: 447.0,
@@ -49,6 +49,17 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ))
                     .toList(),
+              ),
+            ),
+            Positioned(
+              top: 573.0,
+              left: 53.0,
+              width: 286.5,
+              height: 142.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color(0xffc4c4c4),
+                    borderRadius: BorderRadius.circular(0.0)),
               ),
             )
           ],
